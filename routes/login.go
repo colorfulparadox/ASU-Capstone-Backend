19255dd4a83d23f76c9b48c483a90e10d76c4b1a
BluSnake <minewhip@gmail.com>
2024-09-23 14:04:58 -0700

    Changed login.go to use database functions

--- routes/login.go
+++ routes/login.go
@@ -1,19 +1,12 @@
 package routes
 
 import (
-	"context"
+	"BackEnd/database"
 	"fmt"
 	"net/http"
-	"strconv"
-	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5/pgxpool"
-
-	"crypto/sha256"
-	"encoding/base64"
-	"math/rand"
 )
 
 type LoginRequest struct {
@@ -37,37 +30,44 @@ func Login(gc *gin.Context, pool *pgxpool.Pool) {
 		return
 	}
 
-	var username string = ""
-	var password string
+	auth_token := database.Verify_User_Login(loginReq.User, loginReq.Pass)
 
-	pool.QueryRow(
-		context.Background(),
-		"SELECT username, password FROM users WHERE username = $1",
-		loginReq.User,
-	).Scan(&username, &password)
-
-	if loginReq.Pass != password || username == "" {
+	if auth_token == "" {
 		fmt.Println("invalid password")
 		gc.JSON(http.StatusForbidden, "{}")
 	}
 
-	dateIssued := time.Now().Unix()
-	expires := time.Now().Add(2 * 24 * time.Hour).Unix()
+	// var username string = ""
+	// var password string
 
-	//ignore this stuff for now
-	key := uuid.New().String() + "/" + strconv.FormatInt(((int64)(rand.Intn(9000))*expires)+dateIssued^(int64)(rand.Intn(9999)), 16)
-	hash := sha256.New()
-	hash.Write([]byte(key))
-	key64 := base64.URLEncoding.EncodeToString(hash.Sum(nil))
+	// pool.QueryRow(
+	// 	context.Background(),
+	// 	"SELECT username, password FROM users WHERE username = $1",
+	// 	loginReq.User,
+	// ).Scan(&username, &password)
 
-	fmt.Println(key)
-	fmt.Println(key64)
+	// if loginReq.Pass != password || username == "" {
+	// 	fmt.Println("invalid password")
+	// 	gc.JSON(http.StatusForbidden, "{}")
+	// }
 
-	loginToken := LoginToken{
-		AuthID:     key64,
-		DateIssued: dateIssued,
-		Expires:    expires,
-	}
+	// dateIssued := time.Now().Unix()
+	// expires := time.Now().Add(2 * 24 * time.Hour).Unix()
 
-	gc.JSON(http.StatusOK, loginToken)
+	// //ignore this stuff for now
+	// key := uuid.New().String() + "/" + strconv.FormatInt(((int64)(rand.Intn(9000))*expires)+dateIssued^(int64)(rand.Intn(9999)), 16)
+	// hash := sha256.New()
+	// hash.Write([]byte(key))
+	// key64 := base64.URLEncoding.EncodeToString(hash.Sum(nil))
+
+	// fmt.Println(key)
+	// fmt.Println(key64)
+
+	// loginToken := LoginToken{
+	// 	AuthID:     key64,
+	// 	DateIssued: dateIssued,
+	// 	Expires:    expires,
+	// }
+
+	gc.JSON(http.StatusOK, auth_token)
 }
